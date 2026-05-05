66658e241e6c02bd777dcab33b1d938d90fddf6d
Matheus Felipe Zanin <matt_z6@hotmail.com>
2026-05-05 10:29:30 -0300

    chore(layout): load poppins font family

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,37 @@
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  useFonts,
+} from "@expo-google-fonts/poppins";
 import { Stack } from "expo-router";
+import * as ExpoSplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
 import { Provider } from "@/contexts/provider";
-
 import { useTheme } from "@/hooks/use-theme";
 
+ExpoSplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      ExpoSplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <Provider>
       <RootStack />

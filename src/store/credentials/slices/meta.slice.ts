a3e6d45c7f25bcf618a9842b77182e62647f5e2a
Matheus Felipe Zanin <matt_z6@hotmail.com>
2026-05-08 20:59:55 -0300

    chore(store): add new credential to the end of the list

--- src/store/credentials/slices/meta.slice.ts
+++ src/store/credentials/slices/meta.slice.ts
@@ -37,7 +37,7 @@ export const createCredentialsMetaSlice: StateCreator<
 
     const credentialsMeta = get().credentialsMeta;
 
-    const updatedCredentialsMeta = [newCredentialMeta, ...credentialsMeta];
+    const updatedCredentialsMeta = [...credentialsMeta, newCredentialMeta];
 
     set({ credentialsMeta: updatedCredentialsMeta });
   },

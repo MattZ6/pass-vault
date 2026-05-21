4c31e872c3549fa0eb69f03c0914faebf195a15d
Matheus Felipe Zanin <matt_z6@hotmail.com>
2026-05-21 16:30:19 -0300

    refactor: remove unused code

--- src/components/settings/AboutSection/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./ChangelogItem";
-export * from "./InstallationDateItem";
-export * from "./LastUpdateDateItem";
-export * from "./LicensesItem";
-export * from "./NameItem";
-export * from "./VersionItem";
